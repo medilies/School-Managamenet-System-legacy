dbfeeb5382bc69640b21187c15387e970d174fea
medilies <medilies@gmail.com>
2021-12-30 22:44:40 +0100

    cleaning code: client+student creation forms

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -18,51 +18,35 @@
                         <div class="flex flex-wrap -mx-3 mb-6 ">
 
                             <x-forms.input name="fname" type="text">
-                                <x-slot name="label_text">
-                                    Prénom
-                                </x-slot>
+                                <x-slot name="label_text"> Prénom </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="lname" type="text">
-                                <x-slot name="label_text">
-                                    Nom
-                                </x-slot>
+                                <x-slot name="label_text"> Nom </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="ar_fname" type="text">
-                                <x-slot name="label_text">
-                                    الاسم
-                                </x-slot>
+                                <x-slot name="label_text"> الاسم </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="ar_lname" type="text">
-                                <x-slot name="label_text">
-                                    اللقب
-                                </x-slot>
+                                <x-slot name="label_text"> اللقب </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="address" type="text">
-                                <x-slot name="label_text">
-                                    Adresse
-                                </x-slot>
+                                <x-slot name="label_text"> Adresse </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="email" type="email">
-                                <x-slot name="label_text">
-                                    Email
-                                </x-slot>
+                                <x-slot name="label_text"> Email </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="phone" type="tel">
-                                <x-slot name="label_text">
-                                    TELEPHONE
-                                </x-slot>
+                                <x-slot name="label_text"> Téléphone </x-slot>
                             </x-forms.input>
 
                             <x-forms.input name="profession" type="text">
-                                <x-slot name="label_text">
-                                    Profession
-                                </x-slot>
+                                <x-slot name="label_text"> Profession </x-slot>
                             </x-forms.input>
 
                         </div>
