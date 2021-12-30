1309aa645d532fc2d58c9e43c87d12fa88e854d6
medilies <medilies@gmail.com>
2021-12-30 19:42:53 +0100

    Refine clients.create.blade formatting + old input

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-
             {{ __('Clients') }}
         </h2>
     </x-slot>
@@ -11,56 +11,62 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 flex ">
 
-
                     <form action={{ route('clients.store') }} method="post" class="w-full  border-2 p-4 ">
-                        @csrf
-                        <div class="flex flex-wrap -mx-3 mb-6 ">
 
+                        @csrf
+
+                        <div class="flex flex-wrap -mx-3 mb-6 ">
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 " for="fname">
                                     Prénom
                                 </label>
                                 <input name="fname"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('fname', '') }}">
                                 @error('fname')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
                                     </div>
                                 @enderror
-
                             </div>
+
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 " for="lname">
                                     Nom
                                 </label>
                                 <input name="lname"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('lname', '') }}">
                                 @error('lname')
-                                    <div class="text-red-500 mt-2 text-sm">
+                                    <div class="  text-red-500 mt-2 text-sm">
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
+
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 "
                                     for="ar_fname">
                                     الاسم
                                 </label>
                                 <input name="ar_fname"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('ar_fname', '') }}">
                                 @error('ar_fname')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
+
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 "
                                     for="ar_lname">
                                     اللقب
                                 </label>
                                 <input name="ar_lname"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('ar_lname', '') }}">
                                 @error('ar_lname')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
@@ -73,44 +79,51 @@
                                     Address
                                 </label>
                                 <input name="address"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('address', '') }}">
                                 @error('address')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
+
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 " for="email">
                                     Email
                                 </label>
                                 <input name="email" type="email"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('email', '') }}">
                                 @error('email')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
+
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 " for="phone">
                                     TELEPHONE
                                 </label>
                                 <input name="phone"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('phone', '') }}">
                                 @error('phone')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
                                     </div>
                                 @enderror
                             </div>
+
                             <div class="w-full md:w-1/2  px-3 mt-2">
                                 <label class="block  tracking-wide text-gray-700 text-xs font-bold mb-2 "
                                     for="profession">
                                     Profession
                                 </label>
                                 <input name="profession"
-                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    value="{{ old('profession', '') }}">
                                 @error('profession')
                                     <div class="text-red-500 mt-2 text-sm">
                                         {{ $message }}
@@ -118,22 +131,19 @@
                                 @enderror
                             </div>
 
-
-
                         </div>
 
 
                         <button type="submit"
-                            class="bg-blue-500 p-3 text-center font-bold text-white rounded-lg float-right mt-10 text-l hover:bg-blue-700 ">Enregistrer</button>
-                    </form>
+                            class="bg-blue-500 p-3 text-center font-bold text-white rounded-lg float-right mt-10 text-l hover:bg-blue-700 ">
+                            Enregistrer
+                        </button>
 
+                    </form>
 
                 </div>
             </div>
         </div>
     </div>
-
-
-
 
 </x-app-layout>
