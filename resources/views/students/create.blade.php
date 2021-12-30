b53311feb1984816fc0420d5e16b6ee8943d777e
ghs-medilies <ghs.medilies@gmail.com>
2021-12-30 14:38:07 +0100

    VALIDATION: error message + old

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -5,16 +5,42 @@
         </h2>
     </x-slot>
 
-            <form action='{{route("students.store")}}' method="post">
-                @csrf
-                <input type="text" name="fname" placeholder="fname">
-                <input type="text" name="ar_fname" placeholder="ar_fname">
-                <input type="text" name="lname" placeholder="lname">
-                <input type="text" name="ar_lname" placeholder="ar_lname">
-                <input type="date" name="bday">
-                <input type="text" name="bplace" placeholder="bplace">
+    <form action='{{ route('students.store') }}' method="post">
 
-                <button type="submit">Submit</button>
-            </form>
+        @csrf
+
+        <input type="text" name="fname" value="{{ old('fname', '') }}" placeholder="Prénom"><br>
+        @error('fname')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <input type="text" name="ar_fname" value="{{ old('ar_fname', '') }}" placeholder="الاسم"><br>
+        @error('ar_fname')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <input type="text" name="lname" value="{{ old('lname', '') }}" placeholder="Nom"><br>
+        @error('lname')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <input type="text" name="ar_lname" value="{{ old('ar_lname', '') }}" placeholder="اللقب"><br>
+        @error('ar_lname')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <input type="date" name="bday" value="{{ old('bday', '') }}"><br>
+        @error('bday')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <input type="text" name="bplace" value="{{ old('bplace', '') }}" placeholder="bplace"><br>
+        @error('bplace')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <button type="submit">Submit</button><br>
+
+    </form>
 
 </x-app-layout>
