22cde3d7e507e4c69bad25a17fc2f73b2debab13
ghs-medilies <ghs.medilies@gmail.com>
2022-02-12 11:52:17 +0100

    Style: Logo + Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,15 +25,18 @@
         <aside class="bg-blue-900 h-screen w-72 p-4 shadow-md relative">
 
             {{-- LOGO --}}
-            <div class="m-1">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+            <div class="m-2 flex flex-wrap items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-16 w-16" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
-                    <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                    <path d=" M12 14l9-5-9-5-9 5 9 5z" />
                     <path
                         d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                 </svg>
+                <div class="text-white text-lg font-bold flex-1">
+                    School Management System
+                </div>
             </div>
 
             <nav class="overflow-auto max-h-80%">
@@ -122,6 +125,11 @@
         </div>
 
     </div>
+
+    <footer
+        class="absolute bottom-0 right-0 bg-gray-400 bg-opacity-80 text-gray-100 p-1 w-96 rounded-tl-xl text-xs text-right">
+        &copy;2022 <span class="font-bold"> Boudouma Mohamed Ilies </span> & Abdi Mohamed - Elhayat School
+    </footer>
 </body>
 
 </html>
